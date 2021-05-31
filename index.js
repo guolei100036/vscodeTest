bf59592044ca3087e46ee38588381147e0cb92ee
guolei100036 <260341329@qq.com>
2021-05-31 09:11:30 +0800

    mac上测试

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const a=1;
-console.log("🚀 ~ file: index.js ~ line 2 ~ a", a)
+const a = 1;
+console.log("🚀 ~ file: index.js ~ line 2 ~ a", a);
+console.log("🚀 ~ file: index.js ~ line 2 ~ a", a);
+console.log("");
+
 //ctrl + alt + l 选中变量之后，使用这个快捷键生成 console.log
